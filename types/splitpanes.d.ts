c54084edd3cfd2f20e31547873378a0fb19dedaf
Jan Müller <janmueller3698@gmail.com>
2021-09-15 11:29:35 +0200

    fix: update `splitpanes` augment

--- types/splitpanes.d.ts
+++ types/splitpanes.d.ts
@@ -1,4 +1,5 @@
 declare module 'splitpanes' {
-  export const Splitpanes: any
-  export const Pane: any
+  import { Component } from 'vue/types/options'
+  export const Splitpanes: Component<any, any, any, any>
+  export const Pane: Component<any, any, any, any>
 }
