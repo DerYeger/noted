d15f147b34cfec9d4d3832f04097c90c721912f5
Jan Müller <janmueller3698@gmail.com>
2021-09-15 16:41:18 +0200

    feat(design): use gradient as background

--- components/atoms/AppFrame.vue
+++ components/atoms/AppFrame.vue
@@ -6,7 +6,11 @@
 
 <style scoped>
 main {
-  background: var(--color-primary);
+  background: conic-gradient(
+    from 3rad at 105% 50%,
+    var(--color-primary),
+    var(--color-secondary-darken)
+  );
   height: 100%;
 }
 </style>
