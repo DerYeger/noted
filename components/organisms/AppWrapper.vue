2b8b4a1abcfa875a438a80842c4274327ea3e913
Jan Müller <janmueller3698@gmail.com>
2021-09-19 21:42:11 +0200

    feat(settings): add `tabSize` setting

---
+++ components/organisms/AppWrapper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app__wrapper" :style="{ '--tab-size': tabSize }">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  computed: {
+    tabSize(): number {
+      return this.$store.getters['settings/tabSize']
+    },
+  },
+})
+</script>
+
+<style scoped>
+.app__wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+</style>
--- store/settings.ts
+++ store/settings.ts
@@ -1,3 +1,17 @@
-export interface Settings {}
+export interface Settings {
+  tabSize: number
+}
 
-export const state: () => Settings = () => ({})
+export const state: () => Settings = () => ({
+  tabSize: 10,
+})
+
+export const getters = {
+  tabSize: (state: Settings): number => state.tabSize,
+}
+
+export const mutations = {
+  setTabSize(state: Settings, tabSize: number) {
+    state.tabSize = tabSize
+  },
+}
