0a9f9db9967dea2ecc1ff939d53bee62d84358a6
Jan Müller <janmueller3698@gmail.com>
2021-09-19 21:06:50 +0200

    chore: format code

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,10 +11,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'prism-theme-vars',
-    '~/assets/css/main',
-  ],
+  css: ['prism-theme-vars', '~/assets/css/main'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
