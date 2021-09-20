4944869f85dcc84b1def892083294d1e76e71c2e
Jan Müller <janmueller3698@gmail.com>
2021-09-20 19:16:02 +0200

    fix(color-mode): keep theme color after page navigation

---
+++ components/atoms/ThemeColorSetter.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  head() {
+    return {
+      meta: [
+        {
+          hid: 'theme-color',
+          name: 'theme-color',
+          content: this.themeColor as string,
+        },
+      ],
+    }
+  },
+  computed: {
+    themeColor(): string {
+      // force dependency on $colorMode.preference
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const colorMode = this.$colorMode.preference
+      const style = getComputedStyle(document.body)
+      return style.getPropertyValue('--color-background')
+    },
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <AppWrapper>
+    <ClientOnly>
+      <ThemeColorSetter />
+    </ClientOnly>
     <AppHeader>
       <HamburgerButton
         v-model="sidebarOpen"
@@ -45,19 +48,6 @@ export default defineComponent({
         },
       ],
     }
-  },
-  computed: {
-    colorMode(): string {
-      return this.$colorMode.preference
-    },
-  },
-  watch: {
-    colorMode() {
-      const style = getComputedStyle(document.body)
-      const color = style.getPropertyValue('--color-background')
-      const tag = document.querySelector('meta[name="theme-color"]')
-      tag?.setAttribute('content', color)
-    },
   },
 })
 </script>
