fc37fa7ddbc159176d2ac99ae22e281414f9789d
Jan Müller <janmueller3698@gmail.com>
2021-09-22 11:12:53 +0200

    feat(design): add custom checkbox

--- components/atoms/Checkbox.vue
+++ components/atoms/Checkbox.vue
@@ -14,3 +14,34 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+input[type='checkbox'] {
+  all: unset;
+  background: var(--color-darken-1);
+  border-radius: var(--border-radius-medium);
+  box-shadow: var(--shadow-depressed);
+  transition: background var(--anim-medium) ease;
+}
+
+input[type='checkbox'],
+input[type='checkbox']:checked::after {
+  display: inline-flex;
+  height: 1rem;
+  width: 1rem;
+}
+
+input[type='checkbox']:checked {
+  background: var(--color-secondary);
+  box-shadow: var(--shadow-elevated);
+}
+
+input[type='checkbox']:checked::after {
+  align-items: center;
+  content: '✕';
+  font-size: var(--fs-small);
+  justify-content: center;
+  position: relative;
+  text-align: center;
+}
+</style>
